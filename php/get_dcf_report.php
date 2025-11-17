8c21399abaa54718dbfcaa0a7105c2117da2e6f5
nklharish1 <nklharish1@gmail.com>
2025-11-17 10:36:44 +0530

    exp

--- php/get_dcf_report.php
+++ php/get_dcf_report.php
@@ -2,7 +2,8 @@
  include 'db_head.php';
 
 
-
+ $customer_query = ($_GET['customer']) == '' ? 1 : "cus.cus_id  = " . ($_GET['customer']);
+ $dcrf_sts_query = ($_GET['dcf_sts']) == '' ? 1 : "dcf1.sts  = " . ($_GET['dcf_sts']);
  
  
 function test_input($data) {
@@ -13,8 +14,68 @@ $data = "'".$data."'";
 return $data;
 }
 
+$sql = <<<SQL
+WITH
+    dcf_opid AS(
+    SELECT DISTINCT
+        opid,
+        dcf.dcf_id
+    FROM
+        assign_product ap
+    INNER JOIN dcf ON ap.dcf_id = dcf.dcf_id
+    WHERE
+        1
+),
+sop_details AS(
+    SELECT
+        dcf_opid.*,
+        oid
+    FROM
+        dcf_opid
+    INNER JOIN sales_order_product sop ON
+        dcf_opid.opid = sop.opid
+    ORDER BY
+        dcf_id
+),
+sof_details AS(
+    SELECT
+        sop_details.*,
+        sof.customer_id,
+        sof.order_no,
+        sof.emp_id,
+    sp.product
+    
+    FROM
+        sop_details
+    INNER JOIN sales_order_form sof ON
+        sof.oid = sop_details.oid
+       INNER JOIN sales_product sp ON
+        sp.opid = sop_details.opid
+),
+final AS(
+    SELECT
+        sd.*,
+        cus.cus_name,
+        emp.emp_name
+    FROM
+        sof_details sd
+    INNER JOIN customer cus ON
+        cus.cus_id = sd.customer_id
+    INNER JOIN employee emp ON
+        emp.emp_id = sd.emp_id  where $customer_query
+),
+dcf_final as (SELECT
+    dcf_id,    JSON_ARRAYAGG(
+        JSON_OBJECT('order_no',order_no,'cus_name',cus_name,'product',product,'emp','emp_name')) as sale_order
+FROM
+    final GROUP by dcf_id)
+    
+    SELECT dcf_final.sale_order, dcf1.dcf_id,dcf1.dated,dcf1.consignee,dcf1.sts,DATE_FORMAT(dcf1.dated, '%d-%m-%Y') as dated,(SELECT employee.emp_name from employee WHERE employee.emp_id = dcf1.dcf_by) as emp_name  from dcf_final inner join dcf dcf1 on dcf_final.dcf_id = dcf1.dcf_id WHERE  $dcrf_sts_query ORDER by dcf1.dcf_id DESC
+SQL;
 
- $sql = "SELECT dcf.dcf_id,dcf.dated,dcf.consignee,dcf.sts,DATE_FORMAT(dcf.dated, '%d-%m-%Y') as dated,employee.emp_name FROM dcf INNER join employee on dcf.dcf_by = employee.emp_id WHERE 1 ORDER by dcf_by ";
+
+
+//  $sql = "SELECT  dcf1.dcf_id,dcf1.dated,dcf1.consignee,dcf1.sts,DATE_FORMAT(dcf1.dated, '%d-%m-%Y') as dated,employee.emp_name FROM dcf dcf1 INNER join employee on dcf1.dcf_by = employee.emp_id WHERE 1 ORDER by dcf_by ";
 
 $result = $conn->query($sql);
 
@@ -30,5 +91,7 @@ if ($result->num_rows > 0) {
 $conn->close();
 
  ?>
+
+
 
 
