c223f53ed2286e2219b09817654c04ddebfd2b7e
nklharish1 <nklharish1@gmail.com>
2026-01-07 11:37:34 +0530

    customer price php updated

--- php/get_customer_price.php
+++ php/get_customer_price.php
@@ -17,7 +17,8 @@ $sql = "SELECT JSON_OBJECT(
             'sgt_price_id', subgroup_type_price.sgt_price_id,
             'max_price', subgroup_type_price.max_price,
             'min_price', subgroup_type_price.min_price,
-            'mrp', subgroup_type_price.mrp
+            'mrp', subgroup_type_price.mrp,
+            'mtid', $mtid 
         ))
         FROM customer_subgroup_master cus_sub
         LEFT JOIN subgroup_type_price ON cus_sub.sub_group_id = subgroup_type_price.sub_group_id 
@@ -43,7 +44,9 @@ $sql = "SELECT JSON_OBJECT(
             'main_price', sub.price,
             'is_reduce', sub.is_reduce,
             'gsp_id', gstp.gsp_id,
-            'price', gstp.price
+            'price', gstp.price,
+            'group_id', $group_id
+           
         ))
         FROM jaysan_model_subtype sub 
         LEFT JOIN group_subtype_price gstp ON sub.msid = gstp.msid AND gstp.group_id = $group_id  
@@ -51,19 +54,14 @@ $sql = "SELECT JSON_OBJECT(
     ),
     'subgroup_subtypes', (
         SELECT JSON_ARRAYAGG(JSON_OBJECT(
-            'msid', sub.msid,
-            'subtype_name', sub.subtype_name,
-            'main_price', sub.price,
-            'price', sstp.price,
-            'sgst_id', sstp.sgst_id,
-            'sub_group_id', sstp.sub_group_id,
-            'sub_group_name', (SELECT sub_mas.sub_group_name FROM customer_subgroup_master sub_mas WHERE sub_mas.sub_group_id = sstp.sub_group_id)
+            'msid', jaysan_model_subtype.msid,
+            'subtype_name', jaysan_model_subtype.subtype_name,
+            
+            'price_details', (SELECT JSON_ARRAYAGG(JSON_OBJECT('sub_group_name', customer_subgroup_master.sub_group_name,'sub_group_id', customer_subgroup_master.sub_group_id,'price', subgroup_subtype_price.price)) FROM customer_subgroup_master
+ left join subgroup_subtype_price on customer_subgroup_master.sub_group_id = subgroup_subtype_price.sub_group_id and  subgroup_subtype_price.msid = jaysan_model_subtype.msid
+    WHERE customer_subgroup_master.group_id = $group_id)
         ))
-        FROM jaysan_model_subtype sub 
-        LEFT JOIN subgroup_subtype_price sstp ON sub.msid = sstp.msid AND sstp.sub_group_id IN(
-            SELECT cus_sub.sub_group_id FROM customer_subgroup_master cus_sub WHERE cus_sub.group_id = $group_id
-        ) 
-        WHERE sub.mtid = $mtid
+        FROM jaysan_model_subtype WHERE mtid = $mtid
     )
 ) as json_data";
 
