b05e1276affa6861377ed89bdc9facb2bbec9609
sanjayvilvaskan <sanjay040611@gmal.com>
2026-05-04 17:30:20 +0530

    s

--- pdf_handler.php
+++ pdf_handler.php
@@ -54,16 +54,8 @@ $options->set('defaultFont', 'DejaVu Sans');
 $dompdf = new Dompdf($options);
 
 // ========= Load External CSS =========
-
-$bootstrapPath = __DIR__ . '/bootstrap.min.css';
-$invoiceCssPath = __DIR__ . '/invoice_style.css';
-
-$bootstrapCss = file_exists($bootstrapPath) ? file_get_contents($bootstrapPath) : '';
-$customCss = file_exists($invoiceCssPath) ? file_get_contents($invoiceCssPath) : '';
-
-$css = $bootstrapCss . "\n" . $customCss;
-// $cssPath = __DIR__ . '/invoice_style.css';
-// $css = file_exists($cssPath) ? file_get_contents($cssPath) : '';
+$cssPath = __DIR__ . '/invoice_style.css';
+$css = file_exists($cssPath) ? file_get_contents($cssPath) : '';
 
 $css = str_replace(
     ['80px', '40px', '50px', '40px'],
