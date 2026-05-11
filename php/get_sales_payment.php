02a1d75ec436a11c7b5a6c68509d27c7df85e589
nklharish1 <nklharish1@gmail.com>
2026-05-11 12:58:50 +0530

    sales payment updated

--- php/get_sales_payment.php
+++ php/get_sales_payment.php
@@ -15,7 +15,34 @@ return $data;
 }
 
 
- $sql = "SELECT  concat('₹',FORMAT(jaysan_payment.amount, 0)) as amount,DATE_FORMAT(jaysan_payment.payment_date, '%d-%m-%Y %h:%i %p') as date_f ,jaysan_payment.sts,(SELECT sales_order_form.order_no from sales_order_form WHERE sales_order_form.oid = jaysan_payment.oid) as order_no from jaysan_payment WHERE jaysan_payment.sts = 'approved' and jaysan_payment.oid in (SELECT sales_order_form.oid from sales_order_form WHERE sales_order_form.order_no in ( $oid_arr )) order by order_no";
+ $sql = "SELECT concat(
+        '₹', FORMAT(jaysan_payment.amount, 0)
+    ) as amount, DATE_FORMAT(
+        jaysan_payment.payment_date, '%d-%m-%Y %h:%i %p'
+    ) as date_f, jaysan_payment.sts, sales_order_form.order_no,jaysan_payment.ref_no,jaysan_payment.utr_no
+from jaysan_payment
+    inner JOIN sales_order_form on jaysan_payment.oid = sales_order_form.oid
+WHERE
+    jaysan_payment.sts = 'approved'
+    and sales_order_form.order_no in ($oid_arr)
+
+    UNION ALL
+
+
+SELECT concat(
+        '₹', FORMAT(sale_payment_advance.amount, 0)
+    ) as amount, DATE_FORMAT(
+        sale_payment_advance.dated, '%d-%m-%Y %h:%i %p'
+    ) as date_f, concat('advance taken from - ', sf1.order_no) as sts, sales_order_form.order_no,jaysan_payment.ref_no,jaysan_payment.utr_no
+from sale_payment_advance 
+    inner JOIN sales_order_form on sale_payment_advance.oid = sales_order_form.oid and advance_ref_id > 0
+    inner join sale_payment_advance s1 on sale_payment_advance.advance_ref_id = s1.advance_id
+      inner join jaysan_payment on s1.payment_id = jaysan_payment.payment_id
+    inner join sales_order_form sf1 on s1.oid = sf1.oid 
+  
+WHERE
+   
+     sales_order_form.order_no in ($oid_arr)";
 
 $result = $conn->query($sql);
 
